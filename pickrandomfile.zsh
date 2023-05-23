0a7934dd7893ea4870982ea6c1b7616fecce4c12
Andrew Dunbar <hippytrail@gmail.com>
2023-05-23 19:50:21 +0800

    clang on macos needs glfw before raylib; fix pickrandom bug

--- pickrandomfile.zsh
+++ pickrandomfile.zsh
@@ -10,19 +10,19 @@ files=$(find "$starting_dir" -type f -iname "*.png")
 num_files=$(echo "$files" | wc -l | xargs)
 
 # Choose two random file indices
-index1=$(( RANDOM % num_files ))
-index2=$(( RANDOM % num_files ))
+index1=$(( RANDOM % num_files + 1))
+index2=$(( RANDOM % num_files + 1))
 
 # Make sure the two indices are different
 while [ $index1 -eq $index2 ]
 do
-    index2=$(( RANDOM % num_files ))
+    index2=$(( RANDOM % num_files + 1))
 done
 
 # Get the filenames corresponding to the chosen indices
 file1=$(echo "$files" | sed "${index1}q;d")
 file2=$(echo "$files" | sed "${index2}q;d")
 
-echo "Selected files: $file1 $file2"
+echo "Selected files: '$file1', '$file2'"
 
 ./img2nn $file1 $file2
